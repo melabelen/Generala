d2e5e5242cb954106c60847d8b8dd9fecf1336cc
Franco <scucchiero@gmail.com>
2019-05-14 22:35:21 -0300

    Added new file.

---
+++ variables.py
@@ -0,0 +1,22 @@
+#Lista de los nombres de los jugadores
+names = []
+
+#Lista de los puntos que he han hecho por cada uno de estos juegos, ordenado por jugador.
+
+generala = []
+doblegenerala = []
+full = []
+poker = []
+escalera = []
+uno = []
+dos = []
+tres = []
+cuatro = []
+cinco = []
+seis = []
+
+#Dados del juego
+jugada[0,0,0,0,0]
+
+#Cantidad de jugadas que ha hecho el jugador cuyo turno es el del momento
+tiradas =0
